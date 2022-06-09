6d6bcd37f047d34d1d321cec64627164c806fe5d
adams3141 <Adam.strach@student.uj.edu.pl>
2022-06-09 08:51:35 +0200

    Cleared up the code

--- Axis_looker.py
+++ Axis_looker.py
@@ -19,18 +19,16 @@ def main(*argv):
 
     # Loads an image
     prime = cv.imread(cv.samples.findFile(filename))#, cv.IMREAD_GRAYSCALE)
+    original=np.copy(prime)
 
     #checks if image is loaded
     if prime is None:
         print('Obraz się nie otwiera!')
-        #print('Usage: hough_lines.py [image_name -- default ' + default_file + '] \n')
         return -1
-
 
     #przygotowanie obrazu
     canny=cv.Canny(prime,50,200,None,3)#edge (image, treshold1, treshold2, size for sobel opera  detection gaussian filter and gradient
     gray = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
-    grayP = np.copy(gray)
 
     #wyszukanie lini
     lines=cv.HoughLinesP(canny, 0.1, np.pi / 180, 3, None, 300, 10)#image resolution (1pixedl)#########################################################
@@ -60,27 +58,8 @@ def main(*argv):
 
             #t = 0
             for i in range(0, len(lines)):##sprawdza prostopadłość i długość
-                if (lines[i][0][0] < w / 10 and lines[i][0][2] < w / 10 or lines[i][0][0] > w * 9 / 10 and lines[i][0][
-                    2] > w * 9 / 10 or lines[i][0][1] < h / 10 and lines[i][0][3] < h / 10 or lines[i][0][
-                    1] > h * 9 / 10 and lines[i][0][3] > h * 9 / 10):#wykluczenie krawedzi
-                    continue
-                    #print("1")
                 for j in range(i, len(lines)):
-                    if (lines[j][0][0] < w / 10 and lines[j][0][2] < w / 10 or lines[j][0][0] > w * 9 / 10 and
-                            lines[j][0][2] > w * 9 / 10 or lines[j][0][1] < h / 10 and lines[j][0][3] < h / 10 or
-                            lines[j][0][1] > h * 9 / 10 and lines[j][0][3] > h * 9 / 10):#wykluczenie krawedzi
-                        continue
-                        #print("1")
 
-
-                  #  for k in range(0,2):
-                   #     for l in range(0,2):
-                    #        if(math.dist((lines[i][0][k*2],lines[i][0][k*2+1]),((lines[i][0][l*2],lines[i][0][l*2+1])))<w*9/10):
-                    #            print("")
-                              #  t=1
-                              #  break
-                    #if(t==1):
-                     #   break
                     delxi = lines[i][0][2] - lines[i][0][0]
                     delxj = lines[j][0][2] - lines[j][0][0]
                     delyi = (lines[i][0][3] - lines[i][0][1])
@@ -95,10 +74,6 @@ def main(*argv):
                             num1 = i
                             num2 = j
 
-                #if(t==1):#wykryło krawędź
-                 #   print("t1 w krotce zostanie poprawione!!!")
-                  #  return 1
-
             t=0
             for k in range(0,2):
                 for l in range(0,2):
@@ -107,14 +82,13 @@ def main(*argv):
                         print("upss znalazło te same linie")
                         return 1
 
-                   # print("kl  ",k*2," ",k*2+1," ",l*2," ",l*2+1," ",math.dist((lines[num1][0][k*2],lines[num1][0][k*2+1]),((lines[num2][0][l*2],lines[num2][0][l*2+1]))))
                     if(math.dist((lines[num1][0][k*2],lines[num1][0][k*2+1]),((lines[num2][0][l*2],lines[num2][0][l*2+1])))<w/10):
                         t=1
                         break
 
             if(t==1):
                 print("t1==1 w krótce zostanie poprawione!!!")#wykryło krawedzie
-                return 1
+               # return 1
 
             print("lines  ",lines[num1])
             l1 = lines[num1][0]
@@ -123,73 +97,25 @@ def main(*argv):
             cv.line(prime, (l1[0], l1[1]), (l1[2], l1[3]), (0, 0, 255), 3, cv.LINE_AA)
             cv.line(prime, (l2[0], l2[1]), (l2[2], l2[3]), (0, 0, 255), 3, cv.LINE_AA)
 
-            print("l1 ",l1,"   l2",l2)
-        (w, h) = prime.shape[:2]
-        print(h,"  ",w)
 
-        #(h, w) = image.shape[:2]
-        # cv.circle(image,(h/2,w/2), radius=0, color=(0, 0, 255), thickness=-1)
 
-        l1[3]=h-l1[3]
-        l1[1]=h-l1[1]
-        l2[3]=h-l2[3]
-        l2[1]=h-l2[1]
-        print(l1[0]," ",l1[1]," ",l1[2]," ",l1[3])
-
-        #delx1 = l1[2] - l1[0]
-        #delx2 = l2[2] - l2[0]
-        #dely1 = l1[3] - l1[1]
-        #dely2 = l2[3] - l1[1]
-
-        delx1 = l1[2] - l1[0]
-        delx2 = l2[2] - l2[0]
-        dely1 = l1[3] - l1[1]
-        dely2 = l2[3] - l1[1]
-
-        a1=dely1/delx1
-        a2=dely2/delx2
-        b1=l1[1]-l1[0]*a1
-        b2=l2[1]-l2[0]*a2
-
-       # y10 = l1[1]
-       # y20 = l2[1]
-        #cX=(dely2*delx1*y20-dely1*delx2*y10+y10*delx2*delx1-y20*delx2*delx1)/(delx1*dely2-dely1*delx2)#(dely2*line2[0]/delx2-dely1*line1[0]/delx1+line1[1]-line2[1])/(dely2/delx2-dely1/delx1)
-        #cY=(cX-l2[0])*dely2/delx2+y20
-        cX=(b2-b1)/(a1-a2)
-        cY=a1*cX+b1
-        #print(cX,"  ",w/2,"  ",h/2,"  ",cY)
-        #print("bbb",pt11,"  ",pt12)
-        #prime = cv2.circle(prime, (round((l1[2]+l1[0])/2), round((l1[3]+l1[1])/2)), 100, (255, 0, 0),10)  # środki obrazów i środki osi się nie pokrywają :/
+        cX=(l1[0]+l1[2]+l2[0]+l2[2])/4
+        cY=(l1[1]+l1[3]+l2[1]+l2[3])/4
         prime = cv2.circle(prime, (round(cX),round(cY)), 10, (255, 0, 0), -1)
-
-        if (delx1 != 0 and delx2 != 0):  # brak prostych pionowych
-            print("dziala0")
-
-
-           # cX=(dely2*delx1*y20-dely1*delx2*y10+y10*delx2*delx1-y20*delx2*delx1)/(delx1*dely2-dely1*delx2)#(dely2*line2[0]/delx2-dely1*line1[0]/delx1+line1[1]-line2[1])/(dely2/delx2-dely1/delx1)
-            #cY=(cX-y20)*dely2/delx2+y20
-          #  print("aaaa",,"  ",cY)
-          #  prime = cv2.circle(prime, (round(cX), round(cY)), 1, (255, 0, 0), 10)#środki obrazów i środki osi się nie pokrywają :/
 
         tg_phi=(l1[3]-l1[1])/(l2[2]-l2[0])
         phi=math.atan(tg_phi)*180/math.pi
 
-        #print("phi ",phi)
-
-
-
-
-
-
 
         obroty = (rotate_bound(prime, phi, l1, l2),rotate_bound(prime, phi+90, l1, l2),rotate_bound(prime, phi+180, l1, l2),rotate_bound(prime, phi+270, l1, l2))
-        #cv.imshow("Source", prime)
+        cv.imshow("Original", original)
+        cv.imshow("Source", prime)
         #cv.imshow("Standard Hough Line Transform", gray)
         # cv.imshow("Probabilistic Line Transform", grayP)
-       # cv.imshow("obrocone0", obroty[0])
-       # cv.imshow("obrocone1", obroty[1])
-       # cv.imshow("obrocone2", obroty[2])
-       # cv.imshow("obrocone3", obroty[3])
+        cv.imshow("obrocone0", obroty[0])
+        cv.imshow("obrocone1", obroty[1])
+        cv.imshow("obrocone2", obroty[2])
+        cv.imshow("obrocone3", obroty[3])
         # cv.imshow("sdz",prime)
         cv.waitKey()
 
@@ -241,8 +167,8 @@ def main(*argv):
 #main(r"C:\Users\adams\OneDrive\Pulpit\function_tilted_plot.png")
 #main(r"C:\Users\adams\OneDrive\Pulpit\tilted.png")
 #main(r"C:\Users\adams\OneDrive\Pulpit\function_plot.png")
-main(r"C:\Users\adams\Downloads\plot2.png")
+#main(r"C:\Users\adams\Downloads\plot2.png")
 #main(r"C:\Users\adams\Downloads\output-onlinepngtools.png")
 #main(r"C:\Users\adams\Downloads\simple.png")#tak
-#main(r"C:\Users\adams\Downloads\output-onlinepngtools (2).png")#nie
+main(r"C:\Users\adams\Downloads\output-onlinepngtools (2).png")#nie
 
--- rotate_bound.py
+++ rotate_bound.py
@@ -6,104 +6,23 @@ import cv2 as cv
 import numpy as np
 
 def rotate_bound(image, angle, line1, line2):
+
     (h, w) = image.shape[:2]
-    #cv.circle(image,(h/2,w/2), radius=0, color=(0, 0, 255), thickness=-1)
+    cX=round((line1[0]+line1[2]+line2[0]+line2[2])/4)
+    cY=round((line1[1]+line1[3]+line2[1]+line2[3])/4)
 
+    M = cv2.getRotationMatrix2D((round(cX), round(cY)), -angle, 1.0)#rotation matrix around point, angle,
 
-
-    delx1 = line1[2] - line1[0]
-    delx2 = line2[2] - line2[0]
-    dely1 = line1[3] - line1[1]
-    dely2 = line2[3] - line1[1]
-
-    y10 = line1[1]
-    y20 = line2[1]
-
-    if (delx1 != 0 and delx2 != 0):#brak prostych pionowych
-        print("dziala0")
-
-
-
-       # a1 = dely1 / delx1
-       # a2 = dely2 / delx2
-       # b1 = line1[1] - line1[0] * a1
-       # b2 = line2[1] - line2[0] * a2
-
-     #   y10 = line1[1]
-     #   y20 = line2[1]
-     #   cX=(dely2*delx1*y20-dely1*delx2*y10+y10*delx2*delx1-y20*delx2*delx1)/(delx1*dely2-dely1*delx2)#(dely2*line2[0]/delx2-dely1*line1[0]/delx1+line1[1]-line2[1])/(dely2/delx2-dely1/delx1)
-     #   cY=(cX-line2[0])*dely2/delx2+y20
-      #  cX = (b2 - b1) / (a1 - a2)
-      #  cY = a1 * cX + b1
-
-
-
-
-
-        #cX=(dely2*delx1*line2[0]-dely1*delx2*line1[0]+line1[1]*delx2*delx1-line2[1]*delx2*delx1)/(delx1*dely2-dely1*delx2)#(dely2*line2[0]/delx2-dely1*line1[0]/delx1+line1[1]-line2[1])/(dely2/delx2-dely1/delx1)
-       # cY=(cX-line2[0])*dely2/delx2+line2[1]
-        #print(cX,"  ",w/2,"  ",h/2,"  ",cY)
-        cX=round((line1[0]+line1[2]+line2[0]+line2[2])/4)
-        cY=round((line1[1]+line1[3]+line2[1]+line2[3])/4)
-       # if(dely2>dely1):
-         #   h=dely2
-        #    w=delx1
-       # else:
-        #    h=dely1
-        #    w=delx2
-        #####(w/2,h/2)#(255/2,255/2)#// ((y20 - y10) / ((dely1 / delx1) - (dely2 / delx2)),  (delx2 * dely1)*(y20 - y10) / (( (delx2 * dely1) - (delx1 * dely2) ))+y10)
-        M = cv2.getRotationMatrix2D((round(cX), round(cY)), -angle, 1.0)#rotation matrix around point, angle,
-
-        cos = np.abs(M[0, 0])
-        sin = np.abs(M[0, 1])
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
 
         # compute the new bounding dimensions of the image
-        nW = int((h * sin) + (w * cos))
-        nH = int((h * cos) + (w * sin))
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
 
         # adjust the rotation matrix to take into account translation
-        M[0, 2] += (nW / 2) - cX
-        M[1, 2] += (nH / 2) - cY
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
         # perform the actual rotation and return the image
 
-        return cv2.warpAffine(image, M, (nW, nH))
-
-    else:
-        if (delx2 != 0):#pierwsza prosta jest pionowa
-            print("dziala1")
-
-            (cX, cY) = ((line1[0] + line1[2]) / 2, dely2 / delx2 * (line1[0] + line1[2]) / 2 + line2[1])
-            # grab the rotation matrix (applying the negative of the
-            # angle to rotate clockwise), then grab the sine and cosine
-            # (i.e., the rotation components of the matrix)
-
-            M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
-            cos = np.abs(M[0, 0])
-            sin = np.abs(M[0, 1])
-            # compute the new bounding dimensions of the image
-            nW = int((h * sin) + (w * cos))
-            nH = int((h * cos) + (w * sin))
-            # adjust the rotation matrix to take into account translation
-            M[0, 2] += (nW / 2) - cX
-            M[1, 2] += (nH / 2) - cY
-            # perform the actual rotation and return the image
-            return cv2.warpAffine(image, M, (nW, nH))
-        else:#druga prosta jest pionowa
-            print("dziala2222")
-            (cX, cY) = ((line2[0] + line2[2]) / 2, dely1 / delx1 * (line2[0] + line2[2]) / 2 + line1[1])
-            # grab the rotation matrix (applying the negative of the
-            # angle to rotate clockwise), then grab the sine and cosine
-            # (i.e., the rotation components of the matrix)
-            M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
-            cos = np.abs(M[0, 0])
-            sin = np.abs(M[0, 1])
-            # compute the new bounding dimensions of the image
-            nW = int((h * sin) + (w * cos))
-            nH = int((h * cos) + (w * sin))
-            # adjust the rotation matrix to take into account translation
-            M[0, 2] += (nW / 2) - cX
-            M[1, 2] += (nH / 2) - cY
-            # perform the actual rotation and return the image
-            return cv2.warpAffine(image, M, (nW, nH))
-
-            # print("jednak zero")
+    return cv2.warpAffine(image, M, (nW, nH))
