56d0969a5379914a3e6116876848786570b404d3
adams3141 <Adam.strach@student.uj.edu.pl>
2022-06-11 15:16:38 +0200

    update on rotation after test
    code was cleared, comments were added, unnecesery code deleted, programme was optimized. Passed 9/10 tests. The problematic situation is when we have picture in picture with two diffrent background colors, no simple solution is known so far.

--- Axis_looker.py
+++ Axis_looker.py
@@ -1,155 +1,154 @@
-#import packages
+# import packages
 import sys
 import math
-import cv2
 import cv2 as cv
 import numpy as np
 import argparse
 
-#import functions
+# import functions
 from rotate_bound import rotate_bound
 
 
 def main(*argv):
-    if(len(argv) > 0):
+    if (len(argv) > 0):
         filename = argv[0]
     else:
         print("Nie podano nazwy!")
         return -1
 
     # Loads an image
-    prime = cv.imread(cv.samples.findFile(filename))#, cv.IMREAD_GRAYSCALE)
-    original=np.copy(prime)
+    original = cv.imread(cv.samples.findFile(filename))  # , cv.IMREAD_GRAYSCALE)
+    prime = np.copy(original)
 
-    #checks if image is loaded
+    # checks if image is loaded
     if prime is None:
         print('Obraz się nie otwiera!')
         return -1
 
-    #przygotowanie obrazu
-    canny=cv.Canny(prime,50,200,None,3)#edge (image, treshold1, treshold2, size for sobel opera  detection gaussian filter and gradient
-    gray = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
+    # image preaparation
+    canny = cv.Canny(prime, 50, 200, None,
+                     3)  # edge (image, treshold1, treshold2, size for sobel opera  detection gaussian filter and gradient)
+    gray = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)  # gray scale
 
-    #wyszukanie lini
-    lines=cv.HoughLinesP(canny, 0.1, np.pi / 180, 3, None, 300, 10)#image resolution (1pixedl)#########################################################
+    # line detection
+    lines = cv.HoughLinesP(canny, 0.1, np.pi / 180, 3, None, 100,
+                           10)  # (image,1 px resolution, 1 degrre resolution, treshold, minimum lenghtm max "gap")
 
-    for i in range(0, len(lines)):#rozczytanie linii
-        l = lines[i][0]
-        cv.line(gray, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
-        cv.line(gray, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
 
-    if lines is not None:#mamy linnie działamy
+    if lines is not None:  # if we found lines
 
-        if(len(lines)>1000):#za dużo szkoda czasu
+        if (len(lines) > 1000):  # if there was too many lines ommit
             print('Za dużo linii! spróbój ręcznie')
             return -1
 
-        lineslength=len(lines)#jest ok działamy dalej
+        lineslength = len(lines)  # if not, go on
 
         lenghtP = len(lines)
         obrot = np.copy(prime)
-        if lines is not None:  # draw lines
-            print("lines", len(lines))
-            # print(lines)
-            maxlen = 0
-            num1 = 0
-            num2 = 0
-            (h, w) = prime.shape[:2]
 
-            #t = 0
-            for i in range(0, len(lines)):##sprawdza prostopadłość i długość
-                for j in range(i, len(lines)):
+        maxlen = 0
+        num1 = 0
+        num2 = 0
+        (h, w) = prime.shape[:2]
 
-                    delxi = lines[i][0][2] - lines[i][0][0]
-                    delxj = lines[j][0][2] - lines[j][0][0]
-                    delyi = (lines[i][0][3] - lines[i][0][1])
-                    delyj = (lines[j][0][3] - lines[j][0][1])
+        for i in range(0, len(lines)):  #loop over lines to find axis
+            if (lines[i][0][0] < w / 10 and lines[i][0][2] < w / 10 or lines[i][0][0] > w * 9 / 10 and lines[i][0][
+                2] > w * 9 / 10 or lines[i][0][1] < h / 10 and lines[i][0][3] < h / 10 or lines[i][0][
+                1] > h * 9 / 10 and lines[i][0][3] > h * 9 / 10):  # exclusion of boundaries
+                continue
 
-                    leni = np.sqrt(np.power(delxi, 2) + np.power(delyi, 2))
-                    lenj = np.sqrt(np.power(delxj, 2) + np.power(delyj, 2))
+            for j in range(i, len(lines)):
+                if (lines[j][0][0] < w / 10 and lines[j][0][2] < w / 10 or lines[j][0][0] > w * 9 / 10 and
+                        lines[j][0][2] > w * 9 / 10 or lines[j][0][1] < h / 10 and lines[j][0][3] < h / 10 or
+                        lines[j][0][1] > h * 9 / 10 and lines[j][0][3] > h * 9 / 10):  # exclusion of boundaries
+                    continue
 
-                    if (np.abs(delxi * delxj + delyi * delyj) < leni * lenj * 0.05):
-                        if (maxlen < leni + lenj):
-                            maxlen = leni + lenj
-                            num1 = i
-                            num2 = j
+                if (i == j):# dont check perpedicularity of line to itslef
+                    continue
 
-            t=0
-            for k in range(0,2):
-                for l in range(0,2):
+                delxi = lines[i][0][2] - lines[i][0][0]
+                delxj = lines[j][0][2] - lines[j][0][0]
+                delyi = (lines[i][0][3] - lines[i][0][1])
+                delyj = (lines[j][0][3] - lines[j][0][1])
 
-                    if(num1==num2):
-                        print("upss znalazło te same linie")
-                        return 1
-
-                    if(math.dist((lines[num1][0][k*2],lines[num1][0][k*2+1]),((lines[num2][0][l*2],lines[num2][0][l*2+1])))<w/10):
-                        t=1
-                        break
-
-            if(t==1):
-                print("t1==1 w krótce zostanie poprawione!!!")#wykryło krawedzie
-               # return 1
-
-            print("lines  ",lines[num1])
-            l1 = lines[num1][0]
-            l2 = lines[num2][0]
-
-            cv.line(prime, (l1[0], l1[1]), (l1[2], l1[3]), (0, 0, 255), 3, cv.LINE_AA)
-            cv.line(prime, (l2[0], l2[1]), (l2[2], l2[3]), (0, 0, 255), 3, cv.LINE_AA)
+                leni = np.sqrt(np.power(delxi, 2) + np.power(delyi, 2))
+                lenj = np.sqrt(np.power(delxj, 2) + np.power(delyj, 2))
 
 
 
-        cX=(l1[0]+l1[2]+l2[0]+l2[2])/4
-        cY=(l1[1]+l1[3]+l2[1]+l2[3])/4
-        prime = cv2.circle(prime, (round(cX),round(cY)), 10, (255, 0, 0), -1)
+                if (np.abs(delxi * delxj + delyi * delyj) < leni * lenj * 0.03):  # perpendicularity test
+                    t=0
+                    for k in range(0, 2):
+                        for l in range(0, 2):
 
-        tg_phi=(l1[3]-l1[1])/(l2[2]-l2[0])
-        phi=math.atan(tg_phi)*180/math.pi
+                            if (math.dist((lines[i][0][k * 2], lines[i][0][k * 2 + 1]),
+                                          ((lines[j][0][l * 2], lines[j][0][l * 2 + 1]))) < w / 10):
+                                t = 1
+                                break
+                    if (t==0 and maxlen < leni + lenj):
+                        maxlen = leni + lenj
+                        num1 = i
+                        num2 = j
+
+        if (num1 == num2):
+            if (num1 == 0):
+                print("didn't find any perpeniduclar lines, try lowering treshold")
+                return 0
+            else:
+                print("error: perpendiduclar to self")
+                return -1
+        t = 0
+        for k in range(0, 2):
+            for l in range(0, 2):
+
+                if (math.dist((lines[num1][0][k * 2], lines[num1][0][k * 2 + 1]),
+                              ((lines[num2][0][l * 2], lines[num2][0][l * 2 + 1]))) < w / 10):
+                    t = 1
+                    break
+
+        if (t == 1):
+            print("ERROR: one of lines is a boundary, try to define axis by hand")  # wykryło krawedzie
+            return 1
+
+        l1 = lines[num1][0]
+        l2 = lines[num2][0]
+
+        cv.line(original, (l1[0], l1[1]), (l1[2], l1[3]), (0, 0, 255), 3, cv.LINE_AA)  # draw one axis
+        cv.line(original, (l2[0], l2[1]), (l2[2], l2[3]), (0, 0, 255), 3, cv.LINE_AA)  # draw second axis
 
 
-        obroty = (rotate_bound(prime, phi, l1, l2),rotate_bound(prime, phi+90, l1, l2),rotate_bound(prime, phi+180, l1, l2),rotate_bound(prime, phi+270, l1, l2))
-        cv.imshow("Original", original)
-        cv.imshow("Source", prime)
-        #cv.imshow("Standard Hough Line Transform", gray)
-        # cv.imshow("Probabilistic Line Transform", grayP)
-        cv.imshow("obrocone0", obroty[0])
-        cv.imshow("obrocone1", obroty[1])
-        cv.imshow("obrocone2", obroty[2])
-        cv.imshow("obrocone3", obroty[3])
-        # cv.imshow("sdz",prime)
-        cv.waitKey()
+        obroty = rotate_bound(prime, l1, l2)#rotation
+        #return obroty
+        #cv.imshow("Original", original)#testing
+        #cv.imshow("Source0", obroty[0])
+        #cv.imshow("Source1", obroty[1])
+        #cv.imshow("Source2", obroty[2])
+        #cv.imshow("Source", obroty[3])
+        #cv.waitKey()
+        return obroty
 
+        """  # Define the blue colour we want to find - remember OpenCV uses BGR ordering
+                ap = argparse.ArgumentParser()
+                ap.add_argument("-i", "--image", help="path to the image")
+                args = vars(ap.parse_args())
+                # ]
+                boundaries = [
+                    ([0, 0, 0], [255, 0, 0])  # what to show
+                ]
 
+                for (lower, upper) in boundaries:
+                # print()
+                # create NumPy arrays from the boundaries
+                    lower = np.array(lower, dtype="uint8")
+                upper = np.array(upper, dtype="uint8")
+                # print(lower,"  ",upper)
+                #print(cv.inRange(prime, lower, upper))
+                mask = cv.inRange(prime, lower, upper)
+                output = cv.bitwise_and(prime, prime, mask=mask)
 
-
-        # Define the blue colour we want to find - remember OpenCV uses BGR ordering
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-i", "--image", help="path to the image")
-        args = vars(ap.parse_args())
-        # load the image
-
-
-       # boundaries = [
-        #    ([128,128,128], [255, 255, 255])
-        #]
-        boundaries = [
-           ([0,0,0], [255, 0, 0])#what to show
-        ]
-
-        for (lower, upper) in boundaries:
-            #print()
-            # create NumPy arrays from the boundaries
-            lower = np.array(lower, dtype="uint8")
-            upper = np.array(upper, dtype="uint8")
-           # print(lower,"  ",upper)
-            print(cv.inRange(prime, lower, upper))
-            mask=cv.inRange(prime, lower, upper)
-            output = cv.bitwise_and(prime, prime, mask=mask)
-
-            cv2.imshow("images", np.hstack([prime, output]))
-            cv2.waitKey(0)
-
-        return 0
+                cv.imshow("images", np.hstack([prime, output]))
+                cv.waitKey(0)
+                return 0"""
 
     else:
         print("Not a single line detected! try to do it manualy")
@@ -158,17 +157,16 @@ def main(*argv):
 #################################################################Probabilistic
 
 
-   # linesP = cv.HoughLinesP(canny, 1, np.pi / 180, 3, None, 50, 10)
+# linesP = cv.HoughLinesP(canny, 1, np.pi / 180, 3, None, 50, 10)
 
 
-    #cv.imshow('lul',grayP)
-    #cv.waitKey(0)#waits for user interaction
+# cv.imshow('lul',grayP)
+# cv.waitKey(0)#waits for user interaction
 
-#main(r"C:\Users\adams\OneDrive\Pulpit\function_tilted_plot.png")
-#main(r"C:\Users\adams\OneDrive\Pulpit\tilted.png")
-#main(r"C:\Users\adams\OneDrive\Pulpit\function_plot.png")
-#main(r"C:\Users\adams\Downloads\plot2.png")
-#main(r"C:\Users\adams\Downloads\output-onlinepngtools.png")
-#main(r"C:\Users\adams\Downloads\simple.png")#tak
-main(r"C:\Users\adams\Downloads\output-onlinepngtools (2).png")#nie
-
+#main(r"C:\Users\adams\OneDrive\Pulpit\function_tilted_plot.png")#works
+#main(r"C:\Users\adams\OneDrive\Pulpit\tilted.png")#works
+#main(r"C:\Users\adams\OneDrive\Pulpit\function_plot.png")#works
+#main(r"C:\Users\adams\Downloads\plot2.png")#works
+#main(r"C:\Users\adams\Downloads\output-onlinepngtools.png")#works
+#main(r"C:\Users\adams\Downloads\simple.png")  # works
+#main(r"C:\Users\adams\Downloads\output-onlinepngtools (2).png")#nie
--- rotate_bound.py
+++ rotate_bound.py
@@ -5,12 +5,7 @@ import cv2
 import cv2 as cv
 import numpy as np
 
-
-
-    ###############
-
 def rotate_bound(image, line1, line2):
-
     def Rotation_matrix_and_bounds(image, cX, cY, phi):
         M = cv2.getRotationMatrix2D((cX, cY), phi, 1.0)
         cos = np.abs(M[0, 0])
@@ -18,16 +13,13 @@ def rotate_bound(image, line1, line2):
 
         # compute the new bounding dimensions of the image
 
-        nW =round(1.6*w) #int((h * sin) + (w * cos))
-        nH =round(1.6*h) #int((h * cos) + (w * sin))
+        nW = round(1.6 * w)  # int((h * sin) + (w * cos))
+        nH = round(1.6 * h)  # int((h * cos) + (w * sin))
 
         # adjust the rotation matrix to take into account translation
         M[0, 2] += (nW / 2) - cX
         M[1, 2] += (nH / 2) - cY
-        print(M)
-        print()
         return cv2.warpAffine(image, M, (nW, nH))
-
 
     delx1 = line1[2] - line1[0]
     delx2 = line2[2] - line2[0]
@@ -49,66 +41,24 @@ def rotate_bound(image, line1, line2):
         cX = (b2 - b1) / (a1 - a2)
         cY = a1 * cX + b1
 
-        prime = cv.circle(image, (round(cX), round(cY)), 50, (255, 0, 0), -1)
+        #prime = cv.circle(image, (round(cX), round(cY)), 10, (255, 0, 0), 3)# circle  at the rotation invariant point
 
         tg_phi = dely2 / delx2
         phi = math.atan(tg_phi) * 180 / math.pi
 
-
-
-        return [Rotation_matrix_and_bounds(image,cX,cY,phi+x*90) for x in range(0,3)]#cv2.warpAffine(image, M, (nW, nH))
+        return [Rotation_matrix_and_bounds(image, cX, cY, phi + x * 90) for x in
+                range(0, 4)]  # cv2.warpAffine(image, M, (nW, nH))
     else:
         phi = 0
-        if (abs(delx2)<abs(delx1)):  # second line is vertical
-         #   print("dziala1")
-
-            cX=round((line2[0] + line2[2]) / 2) #point of rotation
-            cY=round(dely1 / delx1 *cX+ line1[1])
-
-
-            M = cv2.getRotationMatrix2D((cX, cY), phi, 1.0)#rotation matrix
-            cos = np.abs(M[0, 0])
-            sin = np.abs(M[0, 1])
-
-            # compute the new bounding dimensions of the image
-            nW = int((h * sin) + (w * cos))
-            nH = int((h * cos) + (w * sin))
-
-            # adjust the rotation matrix to take into account translation
-            M[0, 2] += (nW / 2) - cX
-            M[1, 2] += (nH / 2) - cY
-
-            return cv2.warpAffine(image, M, (nW, nH))
+        if (abs(delx2) < abs(delx1)):  # second line is vertical
+            cX = round((line2[0] + line2[2]) / 2)  # point of rotation
+            cY = round(dely1 / delx1 * cX + line1[1])
+            return [Rotation_matrix_and_bounds(image, cX, cY, phi + x * 90) for x in
+                    range(0, 4)]  # cv2.warpAffine(image, M, (nW, nH))
 
         else:  # druga prosta jest pionowa
             cX = round((line1[0] + line1[2]) / 2)  # point of rotation
             cY = round(dely2 / delx2 * cX + line2[1])
 
-            M = cv2.getRotationMatrix2D((cX, cY), phi, 1.0)
-            cos = np.abs(M[0, 0])
-            sin = np.abs(M[0, 1])
+            return [Rotation_matrix_and_bounds(image, cX, cY, phi + x * 90) for x in range(0, 4)]
 
-            # compute the new bounding dimensions of the image
-            nW = int((h * sin) + (w * cos))
-            nH = int((h * cos) + (w * sin))
-
-            # adjust the rotation matrix to take into account translation
-            M[0, 2] += (nW / 2) - cX
-            M[1, 2] += (nH / 2) - cY
-
-            ###############
-
-            M1 = cv2.getRotationMatrix2D((cX, cY), phi+90, 1.0)
-            cos1 = np.abs(M[0, 0])
-            sin1 = np.abs(M[0, 1])
-
-
-            # compute the new bounding dimensions of the image
-            nW1 = int((h * sin1) + (w * cos1))
-            nH1 = int((h * cos1) + (w * sin1))
-
-            # adjust the rotation matrix to take into account translation
-            M1[0, 2] += (nW1 / 2) - cX
-            M1[1, 2] += (nH1 / 2) - cY
-
-            return cv2.warpAffine(image, M, (nW, nH))
